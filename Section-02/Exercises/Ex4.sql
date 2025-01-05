67faaee952ea3c8b94c1584399e95c25b2a7ad0a
Farauk98 <74719318+Farauk98@users.noreply.github.com>
2025-01-05 20:23:29 +0100

    Create Ex4.sql

---
+++ Section-02/Exercises/Ex4.sql
@@ -0,0 +1,11 @@
+-- Write query here to update the 'units_sold' of the phone with name 'N8' to 8543
+
+Update phones
+set units_sold = 8543
+where name = "N8";
+
+
+-- Write query here to select all rows and columns of the 'phones' table
+
+select *
+from phones;
